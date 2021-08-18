062643ea23267edc102588925593d78b9bcafd4f
Andrés Villalobos <andresalvivar@gmail.com>
2021-08-17 22:26:29 -0600

    Thinking really simple for a multiplayer tank game

---
+++ Assets/Game/System/Tank/Tank.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    public int health = 4;
+    public float speed = 1;
+    public float dash = 1;
+}
