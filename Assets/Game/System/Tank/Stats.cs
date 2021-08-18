f352690fad5889f7fe0cd12bffb5eaee516e2467
Andrés Villalobos <andresalvivar@gmail.com>
2021-08-17 23:36:28 -0600

    Order

--- Assets/Game/System/Tank/Stats.cs
+++ Assets/Game/System/Tank/Stats.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Stats : MonoBehaviour
 {
-    public int currentHealth = 4;
     public int health = 4;
+    public int currentHealth = 4;
 
-    public float currentSpeed = 1;
     public float speed = 1;
+    public float currentSpeed = 1;
 
     public Vector3 direction;
 }
