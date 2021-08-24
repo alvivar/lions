9eee0d0d0a3900779a95a4e42a1e10594c2d4163
Andrés Villalobos <andresalvivar@gmail.com>
2021-08-23 18:50:03 -0600

    Diagonals

--- Assets/Game/Code/System/RotateByKeyboard/RotateByKeyboardSystem.cs
+++ Assets/Game/Code/System/RotateByKeyboard/RotateByKeyboardSystem.cs
@@ -12,6 +12,8 @@ public class RotateByKeyboardSystem : MonoBehaviour
             if (r.input.wasd != Vector3.zero)
                 r.input.lastWasd = r.input.wasd;
 
+            // Rects
+
             if (r.input.wasd.x == 1)
                 r.target.transform.localEulerAngles = new Vector3(0, 0, 0);
 
@@ -23,6 +25,20 @@ public class RotateByKeyboardSystem : MonoBehaviour
 
             if (r.input.wasd.y == -1)
                 r.target.transform.localEulerAngles = new Vector3(0, 180, 270);
+
+            // Diagonals
+
+            if (r.input.wasd.x == 1 && r.input.wasd.y == 1)
+                r.target.transform.localEulerAngles = new Vector3(0, 0, 45);
+
+            if (r.input.wasd.x == -1 && r.input.wasd.y == 1)
+                r.target.transform.localEulerAngles = new Vector3(0, 0, 135);
+
+            if (r.input.wasd.x == 1 && r.input.wasd.y == -1)
+                r.target.transform.localEulerAngles = new Vector3(0, 0, 315);
+
+            if (r.input.wasd.x == -1 && r.input.wasd.y == -1)
+                r.target.transform.localEulerAngles = new Vector3(0, 0, 225);
         }
     }
 }
