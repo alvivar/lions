271349c41524070ceef93376ce74a564a8a2a89b
Andrés Villalobos <andresalvivar@gmail.com>
2021-08-23 22:10:33 -0600

    Defaults

--- Assets/Game/Code/Entity/Stats.cs
+++ Assets/Game/Code/Entity/Stats.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class Stats : MonoBehaviour
 {
-    public float speed = 1;
+    public float speed = 10;
+    public float dampening = 5;
     public Vector3 direction;
     public float duration = 0;
 }
