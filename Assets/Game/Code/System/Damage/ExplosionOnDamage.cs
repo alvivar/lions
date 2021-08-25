75fb7e462aab58910db486b31f83d381fa528fa5
Andrés Villalobos <andresalvivar@gmail.com>
2021-08-25 00:28:15 -0600

    Cool explosion

---
+++ Assets/Game/Code/System/Damage/ExplosionOnDamage.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+// #jam
+public class ExplosionOnDamage : MonoBehaviour
+{
+    public List<Stats> explosions = new List<Stats>();
+
+    [Header("By GetComponent")]
+    public Renderer render;
+    public Damage damage;
+
+    private void Start()
+    {
+        render = GetComponent<Renderer>();
+        damage = GetComponent<Damage>();
+        damage.OnDamage += stat => explosions.Add(stat);
+    }
+
+    private void OnEnable()
+    {
+        ExplosionOnDamageSystem.entities.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        ExplosionOnDamageSystem.entities.Remove(this);
+    }
+}
---
+++ Assets/Game/Code/System/Damage/ExplosionOnDamageSystem.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+// #jam
+public class ExplosionOnDamageSystem : MonoBehaviour
+{
+    public static List<ExplosionOnDamage> entities = new List<ExplosionOnDamage>();
+
+    private void Update()
+    {
+        foreach (var e in entities)
+        {
+            if (e.explosions.Count > 0)
+            {
+                var stats = e.explosions[0];
+                e.explosions.RemoveAt(0);
+
+                e.tt("Explosion")
+                    .Reset()
+                    .Loop(1, t =>
+                    {
+                        e.render.material.color = Color.Lerp(e.render.material.color, Color.clear, Easef.EaseIn(t.t));
+                        e.transform.localScale = Vector3.Lerp(e.transform.localScale, Vector3.one * 3, Easef.EaseIn(t.t));
+                    })
+                    .Add(() =>
+                    {
+                        e.render.material.color = Color.white;
+                        e.transform.localScale = Vector3.one;
+                    });
+            }
+        }
+    }
+}
