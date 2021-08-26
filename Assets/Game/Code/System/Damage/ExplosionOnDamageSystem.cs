74af5621fcf62aed7244d7629e5b05d73882c960
Andrés Villalobos <andresalvivar@gmail.com>
2021-08-25 22:41:30 -0600

    Clean up

--- Assets/Game/Code/System/Damage/ExplosionOnDamageSystem.cs
+++ Assets/Game/Code/System/Damage/ExplosionOnDamageSystem.cs
@@ -12,15 +12,21 @@ public class ExplosionOnDamageSystem : MonoBehaviour
         {
             if (e.explosions.Count > 0)
             {
-                var stats = e.explosions[0];
                 e.explosions.RemoveAt(0);
 
                 e.tt("Explosion")
                     .Reset()
                     .Loop(0.8f, t =>
                     {
-                        e.render.material.color = Color.Lerp(e.render.material.color, Color.clear, Easef.EaseIn(t.t));
-                        e.transform.localScale = Vector3.Lerp(e.transform.localScale, Vector3.one * 3, Easef.EaseIn(t.t));
+                        e.render.material.color = Color.Lerp(
+                            e.render.material.color,
+                            Color.clear,
+                            Easef.EaseIn(t.t));
+
+                        e.transform.localScale = Vector3.Lerp(
+                            e.transform.localScale,
+                            Vector3.one * 3,
+                            Easef.EaseIn(t.t));
                     })
                     .Add(() =>
                     {
