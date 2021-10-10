d33dc10c17d65c3fb44cdcc38f0475a71f9ee454
Andrés Villalobos <andresalvivar@gmail.com>
2021-10-10 03:31:09 -0600

    The first ground

--- Assets/Game/Code/System/Cam/Cam.cs
+++ Assets/Game/Code/System/Cam/Cam.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 // #jam
 public class Cam : MonoBehaviour
 {
+    public Vector2 xLimit = new Vector2(-28.37f, 28.37f);
+    public Vector2 yLimit = new Vector2(-15.93f, 15.93f);
+
     private void OnEnable()
     {
         CamSystem.cams.Add(this);
--- Assets/Game/Code/System/Cam/CamSystem.cs
+++ Assets/Game/Code/System/Cam/CamSystem.cs
@@ -15,12 +15,15 @@ public class CamSystem : MonoBehaviour
             foreach (var t in targets)
                 median += t.transform.position;
             median /= targets.Count;
+
+            median.x = Mathf.Clamp(median.x, c.xLimit.x, c.xLimit.y);
+            median.y = Mathf.Clamp(median.y, c.yLimit.x, c.yLimit.y);
             median.z = c.transform.position.z;
 
             c.transform.position = Vector3.Lerp(
                 c.transform.position,
                 median,
-                Time.deltaTime * 0.5f);
+                Time.deltaTime * 0.9f);
         }
     }
 }
