7d241ef50d965c12ac9e705040215a6804a6adda
Andrés Villalobos <andresalvivar@gmail.com>
2022-07-31 22:26:08 -0600

    This program handles data like a string right now. Eventually it will be bytes.

--- Assets/Game/Code/System/Multiplayer/MultiplayerServer.cs
+++ Assets/Game/Code/System/Multiplayer/MultiplayerServer.cs
@@ -19,8 +19,8 @@ public class MultiplayerServer : MonoBehaviour
         this.tt("TryConnection")
             .Add(() =>
             {
-                bite = new Bite("167.99.58.31", 1986);
-                // bite = new Bite("127.0.0.1", 1984);
+                // bite = new Bite("167.99.58.31", 1986);
+                bite = new Bite("127.0.0.1", 1984);
 
                 var uid = SystemInfo.deviceUniqueIdentifier;
                 bite.Send($"! ping from {uid}", r =>
@@ -31,11 +31,22 @@ public class MultiplayerServer : MonoBehaviour
 
                 bite.DataReceived += data =>
                 {
-                    if (data.Length < 6)
+                    if (data.Length <= 5) // Let's ignore responses like OK or KEY? from queries.
                         return;
 
-                    if (OnResponse != null)
-                        OnResponse(data);
+                    var message = Bitf.Str(data).Trim();
+                    var lines = message.Split('\n');
+
+                    Debug.Log($"Byte DataReceived ({data.Length}): {string.Join(" ", data)}");
+                    Debug.Log($"String DataReceived ({message.Length}): {message}");
+
+                    // There is always a new line at the end of the message, and
+                    // sometimes there are more than one message in the data.
+                    foreach (var line in lines)
+                    {
+                        if (OnResponse != null && line.Length > 5)
+                            OnResponse(line);
+                    }
                 };
             })
             .Add(3)
@@ -47,7 +58,7 @@ public class MultiplayerServer : MonoBehaviour
             .Wait(() => connected)
             .Add(t => bite.Send($"+1 app.connections.id", r =>
             {
-                id = Bitf.Long(r, -1);
+                id = Bitf.Long(r);
                 Debug.Log($"User ID: {id}");
             }));
     }
