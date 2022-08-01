733c1688895df71292b3fe5575e89a92b6c99a0f
Andrés Villalobos <andresalvivar@gmail.com>
2022-07-31 22:31:22 -0600

    Better name.

--- Assets/Game/Code/System/Multiplayer/MultiplayerServer.cs
+++ Assets/Game/Code/System/Multiplayer/MultiplayerServer.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using BiteServer;
+using BiteClient;
 using UnityEngine;
 
 // #jam
--- Assets/Game/Code/Tools/Bite/Receiver.cs
+++ Assets/Game/Code/Tools/Bite/Receiver.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Net.Sockets;
 using System.Threading;
 
-namespace BiteServer
+namespace BiteClient
 {
     internal sealed class Receiver
     {
