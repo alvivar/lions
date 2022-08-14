b72dc9b3d5fc5d98ac03e3bbf14b4e6e50a6e8de
Andrés Villalobos <andresalvivar@gmail.com>
2022-08-14 02:25:41 -0600

    Some bite tests.

---
+++ Assets/Game/Code/Tools/Bite/Tests/SendReceiveALot.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using BiteClient;
+using System.Text;
+
+public class SendReceiveALot : MonoBehaviour
+{
+    private Bite bite;
+    private bool connected = false;
+
+    void Start()
+    {
+        // bite = new Bite("167.99.58.31", 1986);
+        bite = new Bite("127.0.0.1", 1984);
+
+        var uid = SystemInfo.deviceUniqueIdentifier;
+        bite.Send($"! ping from {uid}", r =>
+        {
+            connected = true;
+            Debug.Log("SendReceiveALot connected");
+        });
+
+        bite.DataReceived += data =>
+        {
+            var message = Bitf.Str(data).Trim();
+
+            Debug.Log($"Byte received ({data.Length}): {string.Join(" ", data)}");
+            Debug.Log($"String received ({message.Length}): {message}");
+        };
+    }
+
+    [ContextMenu("Test1")]
+    public void Test1()
+    {
+        bite.Send($"#g test", result =>
+        {
+            Debug.Log("#g test received");
+
+            var message = Bitf.Str(result).Trim();
+            Debug.Log($"Test1 # Byte received ({result.Length}): {string.Join(" ", result)}");
+            Debug.Log($"Test1 # String received ({message.Length}): {message}");
+        });
+
+
+        var builder = new StringBuilder();
+        var index = 65;
+
+        for (int i = 0; i < 8192; i++)
+        {
+            index = index > 90 ? 65 : index;
+            builder.Append((char)index);
+            index += 1;
+        }
+
+        string content = builder.ToString();
+        Debug.Log($"Test1 content ({content.Length}): {content}");
+
+        bite.Send($"s test {content}", result =>
+        {
+            var message = Bitf.Str(result).Trim();
+            Debug.Log($"Test1 # Byte received ({result.Length}): {string.Join(" ", result)}");
+            Debug.Log($"Test1 # String received ({message.Length}): {message}");
+        });
+    }
+}
+
