6bd1068745febc4f38e8dde2c7ad113e2622adbc
Andrés Villalobos <andresalvivar@gmail.com>
2022-08-19 02:16:43 -0600

    Better name.

--- Assets/Game/Code/Tools/Bite/Bitf.cs
+++ Assets/Game/Code/Tools/Bite/Bitf.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
 
-/// Utilities to transform between bytes and types.
+/// Utilities to cast bytes.
 
 public static class Bitf
 {
