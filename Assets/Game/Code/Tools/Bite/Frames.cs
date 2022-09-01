076bf4116c35a9bfd446279bdc9058feb94a64ed
Andrés Villalobos <andresalvivar@gmail.com>
2022-09-01 03:36:37 -0600

    The frame is able to handle the protocol by itself.

--- Assets/Game/Code/Tools/Bite/Frames.cs
+++ Assets/Game/Code/Tools/Bite/Frames.cs
@@ -6,16 +6,19 @@ namespace BiteClient
 {
     public class Frame
     {
-        public byte[] Data { get { return data; } }
 
-        public int Size { get { return (data[0] << 8) | data[1]; } }
+        public int ClientId { get { return (data[0] << 8) | data[1]; } }
+        public int MessageId { get { return (data[2] << 8) | data[3]; } }
+        public int Size { get { return (data[4] << 8) | data[5]; } }
+
+        public byte[] Data { get { return data; } }
 
         public byte[] Content
         {
             get
             {
-                var content = new byte[data.Length - 2];
-                Array.Copy(data, 2, content, 0, content.Length);
+                var content = new byte[data.Length - 6];
+                Array.Copy(data, 6, content, 0, content.Length);
                 return content;
             }
         }
@@ -36,6 +39,32 @@ namespace BiteClient
             var newData = new byte[this.data.Length + data.Length];
             Array.Copy(this.data, newData, this.data.Length);
             Array.Copy(data, 0, newData, this.data.Length, data.Length);
+
+            this.data = newData;
+
+            return this;
+        }
+
+        public Frame FromProtocol(int clientId, int messageId, byte[] data)
+        {
+            var headerSize = 6;
+            var header = new byte[headerSize];
+
+            header[0] = (byte)((clientId & 0xFF00) >> 8);
+            header[1] = (byte)((clientId & 0x00FF));
+
+            header[2] = (byte)((messageId & 0xFF00) >> 8);
+            header[3] = (byte)((messageId & 0x00FF));
+
+            var length = data.Length + headerSize;
+            header[4] = (byte)((length & 0xFF00) >> 8);
+            header[5] = (byte)((length & 0x00FF));
+
+            // Concat
+            var newData = new byte[header.Length + data.Length];
+            Array.Copy(header, newData, header.Length);
+            Array.Copy(data, 0, newData, header.Length, data.Length);
+
             this.data = newData;
 
             return this;
