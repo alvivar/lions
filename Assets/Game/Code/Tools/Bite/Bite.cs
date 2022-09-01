5f199c7bfb786e9384ef0eff98e4603e526505aa
Andrés Villalobos <andresalvivar@gmail.com>
2022-09-01 03:49:39 -0600

    The first message is the id assigned by the server. Frames are the format to send stuff.

--- Assets/Game/Code/Tools/Bite/Bite.cs
+++ Assets/Game/Code/Tools/Bite/Bite.cs
@@ -1,17 +1,22 @@
 using System;
 using System.Net.Sockets;
+using System.Text;
 
 namespace BiteClient
 {
     public sealed class Bite
     {
         internal bool Connected { get { return client.Connected; } }
+        internal long ClientId { get { return clientId; } }
         internal event Action<Frame> FrameReceived;
 
         private TcpClient client;
         private NetworkStream stream;
         private Sender sender;
         private Receiver receiver;
+
+        private long clientId;
+        private static int sentId;
 
         internal Bite(string host, int port)
         {
@@ -31,15 +36,23 @@ namespace BiteClient
             }
         }
 
-        internal void Send(string data, Action<Frame> action = null)
+        internal void Send(byte[] data, Action<Frame> action = null)
         {
             if (!Connected)
                 throw new SocketException((int)SocketError.NotConnected);
 
-            sender.Send(data);
+            sentId += 1;
+            var frame = new Frame().FromProtocol((int)clientId, sentId, data);
+            sender.Send(frame);
 
             if (action != null)
                 receiver.React(action);
+        }
+
+        internal void Send(string data, Action<Frame> action = null)
+        {
+            var bytes = Encoding.UTF8.GetBytes(data);
+            Send(bytes, action);
         }
 
         internal void Shutdown()
@@ -53,6 +66,18 @@ namespace BiteClient
 
         private void OnFrameReceived(Frame frame)
         {
+            // The first frame is the client id asigned by the server.
+            if (clientId < 1)
+            {
+                var bigEndian = frame.Content;
+                if (BitConverter.IsLittleEndian)
+                    Array.Reverse(bigEndian);
+
+                clientId = BitConverter.ToInt64(bigEndian, 0);
+
+                return;
+            }
+
             if (FrameReceived != null)
                 FrameReceived(frame);
         }
